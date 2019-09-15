02a7a15760aa529f44ddd72ad86a23cff4329042
marlhakizi <47464258+marlhakizi@users.noreply.github.com>
2019-09-15 16:16:30 -0500

    Create request.py

---
+++ request.py
@@ -0,0 +1,10 @@
+import requests
+
+url = 'http://localhost:12344/predict'
+r = requests.post(url,json=	{
+		"impression_id":"c4ca4238a0b923820dcc509a6f75849b", "impression_time":4,"user_id":3, 
+		"app_code":4, "os_version":"old","is_4G":5,"is_click":5,"server_time":6, "device_type":"android", "session_id":7,
+       "item_id":4, "item_price":4, "category_1":5, "category_2":5, "category_3":5,"product_type":5
+	})
+
+print(r.json())
