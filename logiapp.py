1106453f29f62191cf942db6ee264167bebe3ec4
marlhakizi <47464258+marlhakizi@users.noreply.github.com>
2019-09-16 15:45:39 -0500

    Update logiapp.py

--- logiapp.py
+++ logiapp.py
@@ -10,6 +10,73 @@ clf = joblib.load('logistic.pkl')
 def home():
     return render_template('index.html')
 
+@app.route('/predic', methods=['POST']) # Your API endpoint URL would consist /predic
+def predic():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    train3 = [np.array(int_features)]
+    colcol=['impression_id', 'impression_time','user_id', 'app_code', 'os_version','is_4G']
+    alltrain=train3.reindex(columns=colcol, fill_value=0).drop_duplicates()
+
+    cat_agg=['count','nunique']
+    num_agg=['min','mean','max','sum']
+    agg_col={'server_time':'nunique',
+    'session_id':'nunique','item_price':'mean',
+    'category_3':['nunique','mean'], 'product_type':['nunique','mean']
+    }
+
+    for k in train3.columns:
+        if k.startswith('category_1') or k.startswith('category_2'):
+            agg_col[k]=['sum','mean']
+        elif k.startswith('server'):
+            agg_col[k]=cat_agg
+        elif k.startswith('cumcount'):
+            agg_col[k]=num_agg
+
+
+    untrain=train3.groupby('impression_id').agg(agg_col)
+    untrain.columns=['J_' + '_'.join(col).strip() for col in untrain.columns.values]
+    on=untrain.reset_index()
+    allallu=on.merge(alltrain,how='left',on='impression_id')
+
+
+    allallu.loc[:,'impression_time']=pd.to_datetime(allallu['impression_time'])
+    allallu['Hour']=allallu.loc[:,'impression_time'].dt.hour
+    allallu['Day']=allallu.loc[:,'impression_time'].dt.day
+
+
+    allallu['newHour']=pd.cut(allallu.Hour,bins=[0,6,12,17,23],labels=['Early','Morning','Afternoon','Night'],include_lowest=True)
+
+
+    hou=pd.get_dummies(allallu.newHour)
+    ensemble=pd.concat([allallu,hou],axis=1)
+
+
+    rty=['impression_id', 'item_id','impression_time','user_id','Hour','os_version',
+    'server_time_y', 'impression_id_y','newHour']
+
+    rtrt=[i for i in ensemble.columns if i not in rty]
+    ensemble1=ensemble[rtrt].fillna(0)
+
+
+    prediction = model.predict(ensemble1)
+
+    output = prediction
+    if output == 1:
+        res = "(Ad not clicked)"
+    else:
+        res = "(Ad clicked)"
+
+    return render_template('index.html', prediction_text='The predicted result is: {} '.format(output)+res)
+
+
+
+
+
+
+
 @app.route('/predict', methods=['POST']) # Your API endpoint URL would consist /predict
 def predict():
             #rr=request.json
