9f0f4b7d0b8dadeeb1cc340fc4d30d46f319951a
marlhakizi <47464258+marlhakizi@users.noreply.github.com>
2019-09-17 19:10:10 -0500

    Update logiapp.py

--- logiapp.py
+++ logiapp.py
@@ -24,9 +24,9 @@ def predic():
     for i,k in zip(colui,feat):
         matching[i]=k
     train3=pd.DataFrame.from_dict([matching])
-    colcol=['impression_id', 'impression_time','user_id', 'app_code', 'os_version','is_4G']
+    #colcol=['impression_id', 'impression_time','user_id', 'app_code', 'os_version','is_4G']
     #alltrain=train3.reindex(columns=colcol, fill_value=0).drop_duplicates()
-    alltrain=train3.reindex(columns=colcol)
+    #alltrain=train3.reindex(columns=colcol)
     cat_agg=['count','nunique']
     num_agg=['min','mean','max','sum']
     agg_col={'server_time':'nunique',
@@ -44,6 +44,7 @@ def predic():
 
 
     untrain=train3.groupby('impression_id').agg(agg_col)
+    #untrain=train3
     untrain.columns=['J_' + '_'.join(col).strip() for col in untrain.columns.values]
     on=untrain.reset_index()
     allallu=on.merge(alltrain,how='left',on='impression_id')
